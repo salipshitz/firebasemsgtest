9dd22b621cbb1d08216a824b4857ddb9aeadff7c
Sa'ar <geger40.1@gmail.com>
2017-07-19 20:32:54 -0400

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyALZs9xPOWHcJFCrNyBAWrD_5iE-4H9CVM",
+    authDomain: "woven-proton-156202.firebaseapp.com",
+    databaseURL: "https://woven-proton-156202.firebaseio.com",
+	projectId: "woven-proton-156202",
+	storageBucket: "woven-proton-156202.appspot.com",
+	messagingSenderId: "968249856923"
+};
+firebase.initializeApp(config);
